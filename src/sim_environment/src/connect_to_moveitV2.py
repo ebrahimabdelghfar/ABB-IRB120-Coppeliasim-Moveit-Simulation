dc304a7030bcbbc61a4b400eac694d90ed8aac77
Ebrahim Abdelghfar Ebrahim <81301684+ebrahimabdelghfar@users.noreply.github.com>
2022-10-06 22:18:29 +0200

    some edit

--- src/sim_environment/src/connect_to_moveitV2.py
+++ src/sim_environment/src/connect_to_moveitV2.py
@@ -2,8 +2,6 @@
 import rospy
 import sim
 from sensor_msgs.msg import JointState
-import tf2_ros
-import geometry_msgs.msg
 
 joint=["joint1","joint2","joint3","joint4","joint5","joint6"]
 
@@ -32,30 +30,11 @@ def get_joint_position():
     pos.append(p)
   return pos
 
-def getTransformStamped(obj_handle,name,relTo,relToName):
-  t = geometry_msgs.msg.TransformStamped()
-  _,p=sim.simxGetObjectPosition(ID,obj_handle,relTo,sim.simx_opmode_streaming)
-  _,o=sim.simxGetObjectQuaternion(ID,obj_handle,relTo,sim.simx_opmode_streaming)
-  t.header.frame_id=relToName
-  t.header.stamp=rospy.Time.now()
-  t.child_frame_id = name
-  t.transform.translation.x = p[0]
-  t.transform.translation.y = p[1]
-  t.transform.translation.z = p[2]
-  t.transform.rotation.x = o[0]
-  t.transform.rotation.y = o[1]
-  t.transform.rotation.z = o[2]
-  t.transform.rotation.w = o[3]
-  return t
-
 rospy.init_node("connector")
 
 rospy.Subscriber("/move_group/fake_controller_joint_states",JointState,joint_callback)
 
 joint_pub=rospy.Publisher("/joint_states",JointState,queue_size=1)
-
-br = tf2_ros.TransformBroadcaster()
-
 
 while not rospy.is_shutdown():
   pos=get_joint_position()
@@ -64,9 +43,7 @@ while not rospy.is_shutdown():
   let_send.name=joint
   let_send.position=pos
   joint_pub.publish(let_send)
-  # for i in range(6):
-  #   _,joint_handle=sim.simxGetObjectHandle(ID,joint[i],sim.simx_opmode_oneshot_wait )
-  #   br.sendTransfor(getTransformStamped(joint_handle,joint[i],-1,'world'))
+
 
 
 
