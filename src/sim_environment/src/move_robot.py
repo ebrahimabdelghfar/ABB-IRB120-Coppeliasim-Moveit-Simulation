590c7c34b9df4e4dc895c7ced2f5797cff376435
Ebrahim Abdelghfar Ebrahim <81301684+ebrahimabdelghfar@users.noreply.github.com>
2022-10-19 11:10:43 +0200

    documentation of the code

--- src/sim_environment/src/move_robot.py
+++ src/sim_environment/src/move_robot.py
@@ -2,9 +2,17 @@
 import rospy
 import sim
 from sensor_msgs.msg import JointState
-
+# defining the name of robot joints to be controlled by ROS
 joint=["joint1","joint2","joint3","joint4","joint5","joint6"]
 
+# this function is called at the start to get the ID number for simulator
+'''
+def connect_port(port):
+  argument :
+    port : port number of the simulator
+  return :
+    clientID : ID number of the simulator
+'''
 def connect_port(port):
   sim.simxFinish(-1)
   clientID=sim.simxStart("127.0.0.1",port,True,True,2000,5) #get the client ID
@@ -14,19 +22,31 @@ def connect_port(port):
     print ("disconnected")
   return clientID
 
+# this function is called to set the joint angle of the robot in the simulator received from moveit
+'''
+def joint_callback(data:JointState):
+  argument : 
+    data : JointState message recieved from moveit
+  function :
+    set the joint angle of the robot in the simulator
+  return:
+    None
+'''
 def joint_callback(data:JointState):
     global ID,joint
     for i in range(6):
+      # get the joint handle of the each joint and give the angle to the joint
       _,joint_handle=sim.simxGetObjectHandle(ID,joint[i],sim.simx_opmode_oneshot_wait)
+      # now move the joint to the angle received from moveit
       sim.simxSetJointTargetPosition(ID,joint_handle,data.position[i],sim.simx_opmode_oneshot)
 
-
+#intiate the node 
 rospy.init_node("move_robot")
-
+#define the subscriber
 rospy.Subscriber("/move_group/fake_controller_joint_states",JointState,joint_callback)
-
+#defining the port number and getting the ID number
 ID=connect_port(19999)
-
+#keep the node running
 rospy.spin()
 
 
--- src/sim_environment/src/sense.py
+++ src/sim_environment/src/sense.py
@@ -2,9 +2,17 @@
 import rospy
 import sim
 from sensor_msgs.msg import JointState
-
+# defining the name of robot joints to be controlled by ROS
 joint=["joint1","joint2","joint3","joint4","joint5","joint6"]
 
+# this function is called at the start to get the ID number for simulator
+'''
+def connect_port(port):
+  argument :
+    port : port number of the simulator
+  return :
+    clientID : ID number of the simulator
+'''
 def connect_port(port):
   sim.simxFinish(-1)
   clientID=sim.simxStart("127.0.0.1",port,True,True,2000,5) #get the client ID
@@ -14,8 +22,19 @@ def connect_port(port):
     print ("disconnected")
   return clientID
 
+# defining the port number and getting the ID number
 ID=connect_port(19998)
 
+# this function is used to get the joint angle of the robot in the simulator 
+'''
+def get_joint_position():
+  argument :
+    None
+  function :
+    get the joint angle of the robot in the simulator
+  return:
+    pos : list of joint angles to be published to moveit
+'''
 def get_joint_position():
   pos=[]
   for i in range(6):
@@ -24,18 +43,26 @@ def get_joint_position():
     pos.append(p)
   return pos
 
+# initate the node the moveit 
 rospy.init_node("sensing")
 
-
+# initate the publisher
+'''
+argeument : 
+  topic name : /joint_states
+  message type : JointState
+  queue size : 0 "to ensure that theres no old message in the queue"
+'''
 joint_pub=rospy.Publisher("/joint_states",JointState,queue_size=0)
 
+# now publish the joint angle to moveit
 while not rospy.is_shutdown():
   pos=get_joint_position()
-  let_send=JointState()
-  let_send.header.stamp=rospy.Time.now()
-  let_send.name=joint
-  let_send.position=pos
-  joint_pub.publish(let_send)
+  let_send=JointState() # create a JointState message
+  let_send.header.stamp=rospy.Time.now() # set the time stamp for the current time
+  let_send.name=joint # set the name of the joints
+  let_send.position=pos # set the position of the joints
+  joint_pub.publish(let_send) # publish the message
 
 
 
